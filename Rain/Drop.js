6a12b9b73033c3de781e49acb8aa325248f37806
heshamabdulzaher <heshamabdulzaher@gmail.com>
2019-03-24 12:26:10 +0200

    Add another Rainfall animation

---
+++ Rain/Drop.js
@@ -0,0 +1,31 @@
+class Drop {
+  constructor() {
+    this.speed = 10;
+    this.width = 8;
+    this.pos = createVector(random(0, width), random(0, height));
+    this.acc = createVector();
+    this.vel = createVector();
+  }
+  show() {
+    image(img, this.pos.x, this.pos.y, 10, 14);
+  }
+
+  dropHitTheGround(f) {
+    if (this.pos.y > height - 4) {
+      this.pos.y = random(-height, 0);
+      this.vel.y = 0.2;
+      this.pos.x = random(0, width);
+      ellipse(this.pos.x, this.pos.y, 4);
+    }
+  }
+
+  applyForce(f) {
+    this.acc.add(f);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+}
---
+++ Rain/sketch.js
@@ -0,0 +1,24 @@
+let drops = [];
+let img;
+function preload() {
+  img = loadImage("https://image.flaticon.com/icons/svg/427/427112.svg");
+}
+function setup() {
+  createCanvas(640, 480);
+  for (let i = 0; i < 100; i++) {
+    let drop = new Drop();
+    drops.push(drop);
+  }
+}
+
+function draw() {
+  //   frameRate(20);
+  background(55);
+  let gravity = createVector(0, 0.3);
+  drops.forEach(drop => {
+    drop.show();
+    drop.applyForce(gravity);
+    drop.update();
+    drop.dropHitTheGround(gravity);
+  });
+}
